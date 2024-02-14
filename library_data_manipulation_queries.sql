8b83ef74a80aabe40079b2336059b6b59cedc313
SamZ2718 <98358603+SamZ2718@users.noreply.github.com>
2024-02-13 23:23:29 -0800

    --Added sql file with some starter querie--

---
+++ library_data_manipulation_queries.sql
@@ -0,0 +1,10 @@
+
+
+
+-- Gets all member information --
+SELECT memeberID, first_name and last_name as name, email from Members
+
+
+-- Gets all book information --
+Select bookID, title, ISBN, is_checked_out as Checked_out_status from Books
+
