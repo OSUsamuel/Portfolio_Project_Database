f27f0f046ec7580812a79f94b71ef79327297e46
lymaii <134333201+lymaii@users.noreply.github.com>
2024-02-15 23:28:33 -0800

    Update ddl.sql

--- ddl.sql
+++ ddl.sql
@@ -8,30 +8,38 @@ DROP TABLE IF EXISTS Books;
 
 /* Create database tables */
 
-CREATE TABLE Authors (
-    authorID        INT AUTO_INCREMENT NOT NULL,
-    first_name      VARCHAR(50),
-    last_name       VARCHAR(50),
-    PRIMARY KEY(authorID)
-);
 
-CREATE TABLE Books (
+CREATE OR REPLACE TABLE Books (
     bookID          INT AUTO_INCREMENT NOT NULL,
     title           VARCHAR(120) NOT NULL,
-    authorID        INT,
     ISBN            INT(13) NOT NULL,
-    is_checked_out  VARCHAR(1),
+    isCheckedOut    TINYINT(1),
     PRIMARY KEY(bookID),
-    FOREIGN KEY(authorID) REFERENCES Authors(authorID),
     UNIQUE(bookID, ISBN)
 );
 
+CREATE OR REPLACE TABLE Authors (
+    authorID        INT AUTO_INCREMENT NOT NULL,
+    firstName       VARCHAR(50),
+    lastName        VARCHAR(50),
+    PRIMARY KEY(authorID),
+    UNIQUE(authorID)
+);
+
+CREATE OR REPLACE TABLE BookAuthors(
+    bookID          INT,
+    authorID        INT,
+    PRIMARY KEY(bookID, authorID),
+    FOREIGN KEY(bookID) REFERENCES Books(bookID),
+    FOREIGN KEY(authorID) REFERENCES Authors(authorID)
+);
+
+
 CREATE TABLE Members (
     memberID        INT AUTO_INCREMENT NOT NULL,
-    first_name      VARCHAR(50) NOT NULL,
-    last_name       VARCHAR(50) NOT NULL,
+    firstName       VARCHAR(50) NOT NULL,
+    lastName        VARCHAR(50) NOT NULL,
     email           VARCHAR(100) NOT NULL,
-    checkedOut      INT,
     PRIMARY KEY(memberID)
 );
 
@@ -49,14 +57,14 @@ CREATE TABLE BorrowingTransactions (
 
 /* Populate tables with data */
 
-INSERT INTO Authors (first_name, last_name)
+INSERT INTO Authors (firstName, lastName)
 VALUES
     ('Patrick', 'Rothfuss'),
     ('Brandon', 'Sanderson'),
     ('Jeff', 'Kinney'),
     ('Douglas', 'Adams');
 
-INSERT INTO Books (title, authorID, ISBN, is_checked_out)
+INSERT INTO Books (title, authorID, ISBN, isCheckedOut)
 VALUES
     ('The Name of the Wind', 1, 9780575081406, 0),
     ('The Wise Man''s Fear', 1, 9780756407199, 1),
@@ -64,11 +72,19 @@ VALUES
     ('Diary of a Wimpy Kid', 3, 9782343445239, 0),
     ('The Hitchhiker''s Guide to the Galaxy', 4, 2345464434343, 1);
 
-INSERT INTO Members (first_name, last_name, email, checkedOut)
+INSERT INTO BookAuthors(bookID, authorID)
 VALUES
-    ('Samuel', 'Zink', 'zinksam@oregon.edu', 1),
-    ('Heidi', 'Ly', 'something@oregonstate.edu', 2),
-    ('Sponge', 'Bob', 'squarepants@yahoo.com', 0);
+    (1, 1),
+    (2, 1),
+    (3, 2),
+    (4, 3),
+    (5, 4);
+
+INSERT INTO Members (firstName, lastName, email)
+VALUES
+    ('Samuel', 'Zink', 'zinksam@oregon.edu'),
+    ('Heidi', 'Ly', 'something@oregonstate.edu'),
+    ('Sponge', 'Bob', 'squarepants@yahoo.com');
 
 INSERT INTO BorrowingTransactions (bookID, memberID, dateBorrowed, dateDue)
 VALUES
