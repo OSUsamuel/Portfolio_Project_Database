487f65a1cb0ca0cb783d7e7df95ab3c5a3772241
SamZ2718 <98358603+SamZ2718@users.noreply.github.com>
2024-02-15 22:23:42 -0800

    updated manipulation query--

--- library_data_manipulation_queries.sql
+++ library_data_manipulation_queries.sql
@@ -2,9 +2,41 @@
 
 
 -- Gets all member information --
-SELECT memeberID, first_name and last_name as name, email from Members
+SELECT memeberID, first_name and last_name as name, email from Members;
 
 
 -- Gets all book information --
-Select bookID, title, ISBN, is_checked_out as Checked_out_status from Books
+Select bookID, title, ISBN from Books;
 
+
+-- Gets Author information --
+Select authorID, first_name, last_name from Authors;
+
+-- Gets transaction information --
+Select transactionID, dateBorrowed, dateDue, title, first_name and last_name as Author, member from BorrowingTransactions
+INNER JOIN Members ON BorrowingTransactions.memberID=Members.memberID
+INNER JOIN Books ON BorrowingTransactions.bookID = Books.bookID
+INNER JOIN Authors ON Books.authorID = Author.authorID;
+
+
+
+-- Adds new member --
+INSERT INTO Members (first_name, last_name, email)
+VALUES (:first_name_input, :last_name_input, email_input);
+
+-- Adds new Book --
+INSERT INTO Books (title, authorID, ISBN)
+VALUES (:title_input,:authorID_input :ISBN_input);
+
+
+-- Delete member -- 
+DELETE FROM Members WHERE (first_name = :first_name_input) and (last_name = last_name_input);
+
+
+-- Delete Book -- 
+DELETE FROM Books WHERE (ISBN = :ISBN);
+
+-- Update member --
+UPDATE Members
+SET   first_name = :first_name_input, last_name = :last_name_input, email = :email_input
+WHERE (first_name = :first_name_input) and (last_name = last_name_input);
