eddd952abe4ca61ecfe68f33622ea9fb878182b8
SamZ2718 <98358603+SamZ2718@users.noreply.github.com>
2024-02-17 21:31:33 -0800

    ugh

--- website/app.js
+++ website/app.js
@@ -7,6 +7,13 @@ var express = require('express');   // We are using the express library for the 
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
 PORT        = 9111;                 // Set a port number at the top so it's easy to change in the future
 
+// app.js
+
+// Database
+var db = require('./database/db-connector')
+
+
+
 /*
     ROUTES
 */
@@ -21,3 +28,4 @@ app.get('/', function(req, res)                 // This is the basic syntax for 
 app.listen(PORT, function(){            // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
     console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
 });
+
