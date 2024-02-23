e38d8919ae244d5ddd4933ed174b215db102ad49
lymaii <134333201+lymaii@users.noreply.github.com>
2024-02-22 19:45:36 -0800

    Updated ddl.sql

--- website/database/ddl.sql
+++ website/database/ddl.sql
@@ -1,41 +1,45 @@
+-- Disable foregin key checks and auto commit
 SET FOREIGN_KEY_CHECKS = 0;
 SET AUTOCOMMIT = 0;
 
+-- Drop existing tables if they exist
 DROP TABLE IF EXISTS BorrowingTransactions;
 DROP TABLE IF EXISTS Members;
 DROP TABLE IF EXISTS Authors;
 DROP TABLE IF EXISTS Books;
 
-/* Create database tables */
+-- Create database tables
 
-
+-- Books Table
 CREATE OR REPLACE TABLE Books (
     bookID          INT AUTO_INCREMENT NOT NULL,
     title           VARCHAR(120) NOT NULL,
     ISBN            INT(13) NOT NULL,
     publisherID     INT,
     PRIMARY KEY(bookID),
+    FOREIGN KEY(publisherID) REFERENCES publisherID,
     UNIQUE(bookID, ISBN)
 );
 
+-- Publishers Table
 CREATE OR REPLACE TABLE Publishers (
-    publisherID INT AUTO_INCREMENT NOT NULL,
-    name        VARCHAR(120) NOT NULL,
-    website     VARCHAR(120) NOT NULL,
+    publisherID     INT AUTO_INCREMENT NOT NULL,
+    name            VARCHAR(120) NOT NULL,
+    website         VARCHAR(120) NOT NULL,
     PRIMARY KEY(publisherID),
 
 );
 
+-- Authors Table
 CREATE OR REPLACE TABLE Authors (
     authorID        INT AUTO_INCREMENT NOT NULL,
     firstName       VARCHAR(50),
     lastName        VARCHAR(50),
-    publisherID     INT, 
     PRIMARY KEY(authorID),
-    FOREIGN KEY(publisherID) REFERENCES publisherID,
     UNIQUE(authorID)
 );
 
+-- BookAuthors Table
 CREATE OR REPLACE TABLE BookAuthors(
     bookID          INT,
     authorID        INT,
@@ -45,7 +49,7 @@ CREATE OR REPLACE TABLE BookAuthors(
 );
 
 
-
+-- Members Table
 CREATE TABLE Members (
     memberID        INT AUTO_INCREMENT NOT NULL,
     firstName       VARCHAR(50) NOT NULL,
@@ -54,6 +58,7 @@ CREATE TABLE Members (
     PRIMARY KEY(memberID)
 );
 
+-- BorrowingTransactions Table
 CREATE TABLE BorrowingTransactions (
     transactionID   INT AUTO_INCREMENT NOT NULL,
     bookID          INT NOT NULL,
@@ -66,8 +71,9 @@ CREATE TABLE BorrowingTransactions (
     UNIQUE(transactionID, bookID, memberID)
 );
 
-/* Populate tables with data */
+-- Populate tables with data
 
+-- Insert Authors data
 INSERT INTO Authors (firstName, lastName)
 VALUES
     ('Patrick', 'Rothfuss'),
@@ -75,14 +81,24 @@ VALUES
     ('Jeff', 'Kinney'),
     ('Douglas', 'Adams');
 
-INSERT INTO Books (title, authorID, ISBN, isCheckedOut)
+-- Insert Books data
+INSERT INTO Books (title, authorID, ISBN, publisherID)
 VALUES
-    ('The Name of the Wind', 1, 9780575081406, 0),
-    ('The Wise Man''s Fear', 1, 9780756407199, 1),
-    ('The Way of Kings', 2, 9780765265279, 1),
-    ('Diary of a Wimpy Kid', 3, 9782343445239, 0),
-    ('The Hitchhiker''s Guide to the Galaxy', 4, 2345464434343, 1);
+    ('The Name of the Wind', 1, 9780575081406, 'DAW Books' ),
+    ('The Wise Man''s Fear', 1, 9780756407199, 'DAW Books'),
+    ('The Way of Kings', 2, 9780765265279, 'Tor Books'),
+    ('Diary of a Wimpy Kid', 3, 9782343445239, 'Amulet Books'),
+    ('The Hitchhiker''s Guide to the Galaxy', 4, 2345464434343, 'Harmony Books');
 
+-- Insert Publisher data
+INSERT INTO Publishers (name, website)
+VALUES
+    ('DAW Books', 'https://www.dawbooks.com'),
+    ('Tor Books', 'https://www.tor.com'),
+    ('Amulet Books', 'https://www.abramsbooks.com/amulet/'),
+    ('Harmony Books', 'https://www.harmonybooks.com');
+
+-- Insert BookAuthors data
 INSERT INTO BookAuthors(bookID, authorID)
 VALUES
     (1, 1),
@@ -91,18 +107,21 @@ VALUES
     (4, 3),
     (5, 4);
 
+-- Insert Members data
 INSERT INTO Members (firstName, lastName, email)
 VALUES
     ('Samuel', 'Zink', 'zinksam@oregon.edu'),
     ('Heidi', 'Ly', 'something@oregonstate.edu'),
     ('Sponge', 'Bob', 'squarepants@yahoo.com');
 
+-- Insert BorrowingTransactions data
 INSERT INTO BorrowingTransactions (bookID, memberID, dateBorrowed, dateDue)
 VALUES
     (2, 1, '2024-02-06', '2025-02-06'),
     (3, 2, '2012-01-04', '2012-02-04'),
     (5, 2, '2012-01-04', '2012-02-04');
 
+--Re-enable foreign key checks and commit changes
 SET FOREIGN_KEY_CHECKS = 1;
 COMMIT;
 
