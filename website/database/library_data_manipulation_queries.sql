6319e6f37a182afdbd862057468969d40f297289
lymaii <lyhe@oregonstate.edu>
2024-03-17 17:34:21 -0700

    Update CRUD operations

--- website/database/library_data_manipulation_queries.sql
+++ website/database/library_data_manipulation_queries.sql
@@ -2,23 +2,20 @@
 /**************************************************************************************************************
 Select queries
 **************************************************************************************************************/
--- Gets all member information --
+-- Gets all Member information --
 SELECT memberID, CONCAT(firstName ,  " ", lastName  ) as name, email from Members;
 
--- Gets all book information --
+-- Gets all Book information --
 Select bookID, title, ISBN, publisherID from Books;
 
 -- Gets Author information --
 Select authorID, firstName, lastName from Authors;
 
--- Gets Publisher information --
-Select publisherID, name, website from Publishers;
-
--- Gets transaction information --
+-- Gets Borrowing Transactions information --
 Select transactionID, dateBorrowed, dateDue, bookID, memberID from BorrowingTransactions;
 
--- Gets author information --
-Select authorID, CONCAT(firstName, " ", lastName) as name from Authors;
+-- Gets Publisher information --
+Select publisherID, name, website from Publishers;
 
 -- Selects book with filter --
 Select title, author, ISBN from Books
@@ -37,17 +34,19 @@ Insert queries for "Add New"
 INSERT INTO Members (firstName, lastName, email)
 VALUES (:first_name_input, :last_name_input, :email_input);
 
--- Adds new publisher --
-INSERT INTO Publishers (name, website)
-VALUES (:name_input, :website_input);
+-- Adds new Book --
+INSERT INTO Books (title, ISBN, publisherID)
+VALUES (:title_input, :ISBN_input,(SELECT publisherID from Publishers WHERE name = :name_input));
 
 -- Adds new Author
 INSERT INTO Authors (firstName, lastName)
 VALUES (:firstName_input, :lastName_input);
 
--- Adds new Book --
-INSERT INTO Books (title, ISBN, publisherID)
-VALUES (:title_input, :ISBN_input,(SELECT publisherID from Publishers WHERE name = :name_input));
+-- Adds new Borrowing Transactions
+
+-- Adds new publisher --
+INSERT INTO Publishers (name, website)
+VALUES (:name_input, :website_input);
 
 -- Maps books to authors
 INSERT INTO BookAuthors (bookID, authorID)
@@ -60,14 +59,16 @@ Delete Queries
 Delete FROM BorrowingTransactions where BorrowingTransactions.memberID = :memberID_input;
 DELETE FROM Members where MemberID = :memberID_input;
 
--- Delete Publisher --
-DELETE FROM Publishers WHERE (name = :name_input);
-
 -- Delete Book -- 
 DELETE FROM Books WHERE (ISBN = :ISBN_input);
 DELETE FROM BookAuthors 
     WHERE
         bookID = (SELECT bookID FROM Books WHERE (ISBN = :ISBN_input));
+
+-- Delete Borrowing Transactions --
+    
+-- Delete Publisher --
+DELETE FROM Publishers WHERE (name = :name_input);
 
 /**************************************************************************************************************
 Update Queries
@@ -77,12 +78,21 @@ UPDATE Members
 SET   firstName = :first_name_input, last_name = :last_name_input, email = :email_input
 WHERE (memberID = :id_input);
 
+-- Update Books -- 
+UPDATE Books
+SET title = ':new_title'
+WHERE bookID = :book_id;
+
 -- Update Authors --
 Update Authors
 SET   firstName = :first_name_input, last_name = :last_name_input
 WHERE (firstName = :firstName_input) and (lastName = lastName_input);
 
+-- Update Borrowing Transactions --
+
+
 -- Update Publishers --
 UPDATE Publishers
 SET  name = :name_input, website = :website_input
 WHERE (name = :name_input) and (:website = :website_input)
+
